07c09b2e0d46d544d9d396bcec01aa8ef9844715
G Praveen Kumar <gpraveenkumar6405@gmail.com>
2024-09-20 01:49:34 +0530

    add

--- server.js
+++ server.js
@@ -1,40 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Web3 = require('web3');
-const path = require('path');
-const solc = require('solc');
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
 
+// app.use(express.static('client/src/css'))
+// app.use(express.static(path.join(__dirname, 'client', 'public')))
+app.use(express.static(path.join(__dirname, 'client', 'src')))
 
-const app = express();
-const port = 3000;
-
-// Serve static files from the 'public' directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve static files from the 'client/public' directory
-app.use(express.static(path.join(__dirname, 'Frontend', 'public')));
-
-
-// Serve static files from the 'client/src' directory
-app.use('/src', express.static(path.join(__dirname, 'Frontend', 'src')));
-
-// app.use(express.static(path.join(__dirname, './client')));
-
-
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({ extended: true }));
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'Frontend', 'public', 'login-form.html'));
-});
+  res.sendFile(path.join(__dirname, 'client','public', 'login-form.html'))
+})
 
 
 app.listen(port, () => {
-    console.log("Server is running on port " + port);
-});
-
-
+  console.log(`Example app listening on port ${port}`)
+})
