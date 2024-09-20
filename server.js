2f231eb8d1f76dc8936e980a830cd8d16839d8c5
G Praveen Kumar <gpraveenkumar6405@gmail.com>
2024-09-20 19:07:30 +0530

    add

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const path = require('path');
+
+
+const app = express();
+const port = 3010;
+
+// Serve static files from the 'client/public' directory
+app.use(express.static(path.join(__dirname, 'client', 'public')));
+
+
+// Serve static files from the 'client/src' directory
+app.use('/src', express.static(path.join(__dirname, 'client', 'src')));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login-form.html'));
+});
+
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
+
+
